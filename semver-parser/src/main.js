74744d029abf896f7df020da2e2c0294ea4500f6
ACTCD <101378590+ACTCD@users.noreply.github.com>
2024-06-27 12:32:13 +0800

    build(semver-parser): v1.0.0

---
+++ semver-parser/src/main.js
@@ -0,0 +1,30 @@
+import * as Core from "@actions/core";
+import * as Semver from "semver";
+
+const input = Core.getInput("input");
+
+let v;
+
+if (input) {
+	v = input;
+} else if (process.env.GITHUB_REF_TYPE === "tag") {
+	v = process.env.GITHUB_REF_NAME;
+} else {
+	Core.setFailed("Missing semver input, and the ref type is not tag.");
+	process.exit(1);
+}
+
+const semver = Semver.parse(v);
+// console.debug(semver);
+
+if (semver) {
+	Core.setOutput("semver", semver.version);
+	Core.setOutput("core", `${semver.major}.${semver.minor}.${semver.patch}`);
+	Core.setOutput("major", semver.major);
+	Core.setOutput("minor", semver.minor);
+	Core.setOutput("patch", semver.patch);
+	Core.setOutput("prerelease", semver.prerelease.join("."));
+	Core.setOutput("build", semver.build.join("."));
+} else {
+	Core.setFailed(`Parsing failed: ${v}`);
+}
